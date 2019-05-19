bcae35c9dd18eddba5c1b863003c394fa7c11a5d
melas123 <salemharrathi@gmail.com>
2019-05-19 11:30:56 +0100

    Display Subscriber Count

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const request = require("request-promise");
+const cheerio = require("cheerio");
 
+let checkSubscriberCount = async channelUrl => {
+  let response = await request(channelUrl);
+
+  let $ = cheerio.load(response);
+
+  let subscribersCount = $(
+    '[class="yt-subscription-button-subscriber-count-branded-horizontal subscribed yt-uix-tooltip"]'
+  ).attr("title");
+
+  return subscribersCount;
+};
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  let subscriberCount = 0;
+  (async () => {
+    let youtubeChannelUrl =
+      "https://www.youtube.com/channel/UChy8R5gSDhOR4K0n84kjq2A?view_as=subscriber";
+
+    subscriberCount = await checkSubscriberCount(youtubeChannelUrl);
+    res.render("index", { title: "Express", count: subscriberCount });
+  })();
 });
 
 module.exports = router;
